3f0dabda005bd03b887ccca5d443dac8e046c824
JX_Tee <jinxintee@gmail.com>
2025-09-20 23:11:25 +0800

    Feat: implement Tutorial page layout and components

---
+++ frontend/src/pages/Tutorial/components/AlgorithmInfo/AlgorithmInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './AlgorithmInfo.module.scss';
+
+function AlgorithmInfo() {
+  return (
+    <div className={styles.algorithmInfo}>
+      {/* Your implementation here */}
+    </div>
+  );
+}
+
+export default AlgorithmInfo;
